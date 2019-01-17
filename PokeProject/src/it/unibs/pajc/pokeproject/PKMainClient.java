51c323fe22bfde828f6703acfe673d2ea23f86bf
matte <matte@MATTEO-ZEN>
2019-01-17 16:01:00 +0100

    First client implementation

--- PokeProject/src/it/unibs/pajc/pokeproject/PKMainClient.java
+++ PokeProject/src/it/unibs/pajc/pokeproject/PKMainClient.java
@@ -1,16 +1,30 @@
 package it.unibs.pajc.pokeproject;
 
-public class PKMainClient {
+import java.io.*;
+import java.net.*;
 
+public class PKMainClient {
+	
+	private static Socket socket;
 	private Pokemon chosenPokemon;
 	
 	public static void main(String[] args) {
-		
+		connectToServer();
 
 	}
 	
 	public void sendChosenPokemon(Pokemon chosen) {
 		// implementazione socket 
+	}
+	
+	public static void connectToServer() {
+		try {
+			socket = new Socket("10.0.83.63", 50000);
+			System.out.println("Successfully connected to server at" + socket.getInetAddress());
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
