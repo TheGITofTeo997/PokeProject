24cfafd63d29e926cf5f5f7237d04a36e32b1e6c
Patrick <drunper@gmail.com>
2019-01-17 14:29:27 +0100

    Added sendChosenPokemon method in PKMainClient

--- PokeProject/src/it/unibs/pajc/pokeproject/PKMainClient.java
+++ PokeProject/src/it/unibs/pajc/pokeproject/PKMainClient.java
@@ -2,9 +2,15 @@ package it.unibs.pajc.pokeproject;
 
 public class PKMainClient {
 
+	private Pokemon chosenPokemon;
+	
 	public static void main(String[] args) {
 		
 
+	}
+	
+	public void sendChosenPokemon(Pokemon chosen) {
+		// implementazione 
 	}
 
 }
