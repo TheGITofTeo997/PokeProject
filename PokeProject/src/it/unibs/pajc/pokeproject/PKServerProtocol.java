36ecf7e8d39810bf19622b9dea55273014fc8324
matte <matte@MATTEO-ZEN>
2019-01-17 18:01:19 +0100

    Finished server, now clients need to refresh data

--- PokeProject/src/it/unibs/pajc/pokeproject/PKServerProtocol.java
+++ PokeProject/src/it/unibs/pajc/pokeproject/PKServerProtocol.java
@@ -8,8 +8,8 @@ public class PKServerProtocol extends Thread{
 	private Socket socketPlayer;
 	private BufferedReader fromClient;
 	private PrintWriter toClient;
-	private HashMap<Integer, java.net.Socket> clientList = new HashMap<>();
-	private static int i=0;
+	private static HashMap<Integer, java.net.Socket> clientList = new HashMap<>();
+	private static ArrayList<Integer> clientPorts = new ArrayList<>();
 
 	
 	public PKServerProtocol(Socket socket) {
@@ -19,20 +19,32 @@ public class PKServerProtocol extends Thread{
 	
 	public void run() {
 		System.out.println("Serving client with address "+ socketPlayer.getInetAddress());
-		clientList.put(i++, socketPlayer);
+		clientList.put(socketPlayer.getPort(), socketPlayer);
 		try {		
 			fromClient = new BufferedReader(new InputStreamReader(socketPlayer.getInputStream()));
-			toClient = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socketPlayer.getOutputStream())), true);
-			String request;
-			while((request = fromClient.readLine()) != null) {
-				System.out.println("Server received " + request + " damage from " + socketPlayer.getInetAddress());
-				System.out.println("Sending " + request);
-				toClient.println(request);
-				System.out.println(clientList.get(0));
-				System.out.println(clientList.get(1));
-				
-				
+			String request;		
+			int key=0;
+			for(Iterator<Integer> iter = clientList.keySet().iterator(); iter.hasNext(); )
+			{
+				key = iter.next();		
+				System.out.println(clientList.get(key));
 			}
+			clientPorts.add(key);	
+			for(int j=0;j<clientPorts.size();j++) System.out.println(clientPorts.get(j));
+			while((request = fromClient.readLine()) != null) {
+			if(socketPlayer.getPort() == clientPorts.get(0)) {
+				int port_receiver = clientPorts.get(1);
+				toClient = new PrintWriter(new BufferedWriter(new OutputStreamWriter(clientList.get(port_receiver).getOutputStream())),true);
+				toClient.println(request);
+			}else if(socketPlayer.getPort() == clientPorts.get(1)){
+				int port_receiver = clientPorts.get(0);
+				toClient = new PrintWriter(new BufferedWriter(new OutputStreamWriter(clientList.get(port_receiver).getOutputStream())),true);
+				toClient.println(request);
+			}
+			System.out.println("Server received " + request + " damage from " + socketPlayer.getInetAddress());
+			System.out.println("Sending " + request);
+			toClient.println(request);				
+		}
 		}
 		catch(Exception e)
 		{
