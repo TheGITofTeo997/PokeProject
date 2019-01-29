8d83cd131f12a38de556c6d15773608a3f2bce3d
matte <matte@MATTEO-ZEN>
2019-01-29 17:47:16 +0100

    Unsuccessfully trying to implement images on chooser

--- PokeProject/src/it/unibs/pajc/pokeproject/PokeChooserFrame.java
+++ PokeProject/src/it/unibs/pajc/pokeproject/PokeChooserFrame.java
@@ -28,8 +28,10 @@ public class PokeChooserFrame extends JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel label = new JLabel("");
-		label.setIcon(new ImageIcon(PokeChooserFrame.class.getResource("/bulbasaur-3.gif")));
-		label.setBounds(10, 11, 248, 221);
+		label.setIcon(new ImageIcon(getClass().getResource("/img/bulbasaur-3.gif")));
+		label.setBounds(10, 11, 274, 263);
 		contentPane.add(label);
+		
+		
 	}
 }
