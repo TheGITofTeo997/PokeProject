d931c0fcecfbc33b69a42bdc5463151c331de1e2
matte <matte@MATTEO-ZEN>
2019-02-05 14:49:46 +0100

    Waiting Frame

---
+++ PokeProject/src/it/unibs/pajc/pokeproject/WaitingFrame.java
@@ -0,0 +1,44 @@
+package it.unibs.pajc.pokeproject;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class WaitingFrame extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					WaitingFrame frame = new WaitingFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public WaitingFrame() {
+		setTitle("Waiting...");
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 361, 233);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+	}
+
+}
