892c8a3e597f8b2510c92dddb4262765884d5dfb
Patrick <drunper@gmail.com>
2019-02-19 20:05:30 +0100

    Small fix for issue #20

--- PokeProject/src/it/unibs/pajc/pokeproject/controller/PKClient.java
+++ PokeProject/src/it/unibs/pajc/pokeproject/controller/PKClient.java
@@ -7,7 +7,7 @@ public class PKClient {
 	public static void main(String[] args) {
 		//PKClientModel model = new ClientModel();
 		PKClientWindow view = new PKClientWindow();
-		PKClientController pkClient = new PKClientController();
+		PKClientController pkClient = new PKClientController(view);
 	}
 
 }
